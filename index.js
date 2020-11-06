a84c9f13a82c92fc40b8b47eeac46e7e64c48cc1
emtupp <emilyc.tupper@gmail.com>
2020-11-06 20:18:57 +0000

    Still missing some features considered stretch

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+};
 
-  constructor(amount) {
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
 
-}
+};
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+};
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+};
 
 
 
@@ -18,12 +40,27 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+
+const user1 = new Account('user1');
+
+t1 = new Withdrawal(50.25, user1);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, user1);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, user1);
+t3.commit();
+console.log('Transaction 3:', t3)
+
+console.log('user1 balance:', user1.balance);
+
+// FEATURES:
+// Allow multiple accounts to be created
+// Each account can have many transactions
+// Allow withdrawals and deposits into accounts
+// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
+// Allow us to retrieve the current balance of the account at any time
+// Don't allow withdrawals that exceed the remaining balance of the account
