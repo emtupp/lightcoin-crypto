a0d7d164ea82deddec148dd123d87e0a47519a62
emtupp <emilyc.tupper@gmail.com>
2020-11-24 20:04:50 +0000

    adding in final solution, taken from compass

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 class Account {
 
-  constructor(username) {
-    this.username = username;
-    this.balance = 0;
+  constructor() {
+    this.transactions = [];
   }
-};
 
+  get balance() {
+  	let balance = 0;
+    for (let t of this.transactions) {
+    	balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+  	this.transactions.push(transaction);
+  }
+}
+
+// abstract class
 class Transaction {
 
   constructor(amount, account) {
-    this.amount = amount;
+    this.amount  = amount;
     this.account = account;
   }
 
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
-};
+}
 
 class Withdrawal extends Transaction {
 
@@ -25,42 +39,44 @@ class Withdrawal extends Transaction {
     return -this.amount;
   }
 
-};
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
 class Deposit extends Transaction {
 
   get value() {
     return this.amount;
   }
-};
 
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+}
 
+// DRIVER CODE (yes, keep everything in one file for now... b/c cog load)
+const myAccount = new Account();
 
+console.log('Starting Account Balance: ', myAccount.balance);
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-const user1 = new Account('user1');
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
 
-t1 = new Withdrawal(50.25, user1);
-t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
 
-t2 = new Withdrawal(9.99, user1);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-t3 = new Deposit(120.00, user1);
-t3.commit();
-console.log('Transaction 3:', t3)
-
-console.log('user1 balance:', user1.balance);
-
-// FEATURES:
-// Allow multiple accounts to be created
-// Each account can have many transactions
-// Allow withdrawals and deposits into accounts
-// Allow us to retrieve the transaction history of an account (all withdrawals and deposits)
-// Allow us to retrieve the current balance of the account at any time
-// Don't allow withdrawals that exceed the remaining balance of the account
+console.log('Account Transaction History: ', myAccount.transactions);
